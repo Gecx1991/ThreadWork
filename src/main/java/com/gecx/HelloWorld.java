148eec21b5d7f34146aeb9f7292b63975b778abd
Gecx <a512730501@126.com>
2019-04-17 16:11:44 +0800

    初始化 ThreadWork

---
+++ src/main/java/com/gecx/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.gecx;
+
+/**
+ * @author Gecx
+ * @Description: TODO
+ * @date 2019/4/17 16:08
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

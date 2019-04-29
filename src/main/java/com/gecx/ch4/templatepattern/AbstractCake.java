7caf3ebe609149af8d38359fd4146892944e0587
Gecx <a512730501@126.com>
2019-04-29 10:36:54 +0800

    模板方法模式

--- src/main/java/com/gecx/ch4/templatepattern/AbstractCake.java
+++ src/main/java/com/gecx/ch4/templatepattern/AbstractCake.java
@@ -3,6 +3,8 @@ package com.gecx.ch4.templatepattern;
 /**
  * @author Gecx
  * @Description: 模板方法模式
+ * 模板方法模式的意图是，定义一个操作中的算法的骨架，而将一些步骤的实现延迟到子类中。
+ * 模板方法使得子类可以不改变一个算法的结构即可重定义该算法的某些特定步骤。我们最常见的就是Spring框架里的各种Template
  * @date 2019/4/29 10:07
  */
 public abstract class AbstractCake {
